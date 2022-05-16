76bc476d4a66f524632ce9dee1206e73837aeb2f
brettl1991 <brettl1991@gmail.com>
2022-05-16 14:09:02 +0100

    Displaying a map with our current position using leaflet library

--- script.js
+++ script.js
@@ -17,9 +17,25 @@ if (navigator.geolocation)
     //this func takes 2 callback funcs, 1 is the success callback: once the browser succesfully got the coordiantes of the current position of the user and the 2nd is the error func: when we get an error while getting the coordinates
     function (position) {
       // console.log(position);
+      //Displaying a map using leaflet library
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      console.log(`https://www.google.com/maps/${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //L. is stands for leaflet namespace
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');

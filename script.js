e8199243e9b8ad74537eb0a806bd56922e37310f
brettl1991 <brettl1991@gmail.com>
2022-05-16 13:40:39 +0100

    How to use geo location with your coordinates

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//using geo location
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    //this func takes 2 callback funcs, 1 is the success callback: once the browser succesfully got the coordiantes of the current position of the user and the 2nd is the error func: when we get an error while getting the coordinates
+    function (position) {
+      // console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );

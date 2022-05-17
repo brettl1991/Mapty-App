722059fdc37b45d0628650864f9646a369b4a26f
brettl1991 <brettl1991@gmail.com>
2022-05-17 11:59:01 +0100

    Managing workout data, creating classes

--- script.js
+++ script.js
@@ -11,6 +11,53 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  //date when the obj was created
+  date = new Date();
+  //unique id to use later
+  id = (Date.now() + '').slice(-10); //converting into number
+  constructor(coords, distance, duration) {
+    // this.date=...
+    // this.id=...
+    this.coords = coords; //[lat, lng]
+    this.distance = distance; //for later this should be in km
+    this.duration = duration; //for later this should be in minutes
+  }
+}
+
+//child classes
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
 //Implementing classes based on architecture-part-1
 class App {
   #map;
